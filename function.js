1b05e84e8bb354f74a557c13cd8a60cb5878732d
Ayush <ayushkhetrapal0000@gmail.com>
2024-10-24 15:05:50 +0530

    add: env variables and atlas connect new new new

--- function.js
+++ function.js
@@ -1,18 +1,19 @@
 const multer = require('multer');
 const path = require('path');
 
-// Set up storage configuration for Multer
+// Configure storage for uploaded images
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'merch_images/'); // Directory where images are saved
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + path.extname(file.originalname); // Add a unique timestamp to the filename
-        cb(null, file.fieldname + "_" + uniqueSuffix);
+        // Create a unique filename by combining original name with timestamp and random value
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9); // Generate a random suffix
+        cb(null, `${file.fieldname}_${uniqueSuffix}${path.extname(file.originalname)}`); // Maintain original extension
     }
 });
 
-// Create an upload instance with the storage configuration
+// Create the multer instance
 const upload = multer({ storage: storage });
 
 module.exports = upload;
